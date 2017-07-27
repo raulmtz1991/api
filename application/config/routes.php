dc0bca8e4a66a4978305824f8909d4d1333501d6
Raulmtz <raulmartinezdelrio@hotmail.com>
2017-07-27 01:39:16 -0500

    lists

--- application/config/routes.php
+++ application/config/routes.php
@@ -60,3 +60,6 @@ $route['translate_uri_dashes'] = TRUE;
 */
 $route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
 $route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
+
+
+$route['alergias/(:any)'] = 'lista_alergias/data/id/$1';
